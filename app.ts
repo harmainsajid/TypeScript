cb81365d83c92a2a1482a4abfcf71cb6f0d32de0
harmainsajid <harmainsajid23@gmail.com>
2023-03-09 13:16:29 +0500

    app.ts updated and added

---
+++ app.ts
@@ -0,0 +1,167 @@
+// //Methods to DISPLAY OUTPUT
+// //ALERT
+// // alert("Hello World");
+// // //CONSOLE.LOG
+// // console.log("Hello World");
+// // //DOCUMENT.WRITE
+// // document.write("Hello World");
+
+// //Variables
+// let firstName: string = "Harmain";
+// let lastName: string = "Sajid";
+// console.log(firstName + lastName);
+
+// // VARIABLE NAMES LEGAL & ILLEGAL
+// // legal
+// let $ali;
+// let _ali;
+// let ali;
+// let ali1;
+// let al_i;
+
+// // illegal
+// // 1ali;
+// // @ali;
+// // *ali;
+
+// // COMMENTS
+// //single line comment
+// /* multi
+// line
+// comment */
+
+// // STATEMENT (A group of instructions)
+// let myPet: string = "cat";
+// console.log(myPet);
+
+// // DATA TYPES
+// // premitive data types
+// // String
+// let myName: string = "Hamza";
+// // Number
+// let num: number = 27;
+// // Boolean
+// let isMarried: boolean = false;
+// // Undefined
+// let unassigned: undefined;
+// // Null
+// let empty: null = null;
+
+// console.log(myName);
+// console.log(num);
+// console.log(isMarried);
+// console.log(unassigned);
+// console.log(empty);
+
+// // TEMPLATE LITERALS
+
+// // TYPE OF OPERATORS
+// // analyzing and modifying data types
+// let testVariable: number = 1; // check the type of a variable by entering typeof
+// console.log(typeof testVariable);
+
+// let v1: number = 2; //JavaScript can change types Sometimes JavaScript does this automatically(but not typeScript)
+// let v2: any = "2";
+// console.log(v1 * v2); // 4 ← Type Number
+// console.log(v1 + v2); // “22” ← Type String
+
+// // OPERATORS
+// // ARITHMETIC OPERATORS
+// // additon
+// let n1: number = 1;
+// let n2: number = 2;
+// console.log(n1 + n2); // 3
+
+// let str1: string = "1";
+// let str2: string = "2";
+// console.log(str1 + str2); // “12”
+
+// //subtraction
+// let n1:number = 5;
+// let n2:number = 2;
+// console.log(n1 - n2); // 3
+
+// // multiplication
+// let n1:number = 5;
+// let n2:number = 2;
+// console.log(n1 * n2); // 10
+
+// // division
+// let n1:number = 4;
+// let n2:number = 2;
+// console.log(n1 / n2); // 2
+
+// // exponentiation
+// let n1:number = 2;
+// let n2:number = 2;
+// console.log(n1 ** n2); // 4
+
+// // modulus
+// let n1:number = 10;
+// let n2:number = 3;
+// console.log(n1 % n2); // 1
+
+// // assignment operators (Assignment operator are used to assigning values to variables.)
+// let n:number = 5;
+// console.log(n); // 5
+// n += 5;
+// console.log(n); // 10
+// n -= 5;
+// console.log(n); // 5
+
+// // comparision operators
+// let n = 5;
+// console.log(n == 5); // true
+// console.log(n === 5); // true
+// console.log(n != 5); // false
+// console.log(n > 8); // false
+// console.log(n < 8); // true
+// console.log(n >= 8); // false
+// console.log(n <= 8); // true
+
+// // Math Expressions Familiar Operators
+// let add: number = 2 + 3; // 5
+// let subtraction: number = 8 - 4; // 4
+// let multiplication: number = 2 * 2; // 4
+// let division: number = 4 / 2; // 2
+// let modulus: number = 9 % 3; // 0
+
+// // Math Expressions UnFamiliar Operators
+// // “++” : It increments the variable by 1.
+// // “--” : It decrements the variable by 1.
+// // “**” : Exponentiation is one of the newer operators in JavaScript, and it allows us to calculate the power of a number by its exponent.
+
+// // INCREMENT & DECREMENT
+// // post increment
+// let i:number = 1;
+// let num:number = i++  //  1
+// console.log(num);
+
+// // pre increment
+// let i: number = 1;
+// let num: number = ++i; //  2
+// console.log(num);
+
+// // post decrement
+// let i: number = 1;
+// let num: number = i--; //  1
+// console.log(num);
+
+// // pre decrement
+// let i: number = 2;
+// let num: number = --i; //  1
+// console.log(num);
+
+
+// // Math Expressions Eliminating Ambiguity
+// var totalVal:number = (5 + 2)  *  3 + 6;  // 27 (BODMAS)
+// console.log(totalVal);
+// var totalVal:number = (2 * 4) * 4 + 2; // 34 
+// console.log(totalVal);
+
+// // Concatenating Text String
+// let userName:string  = "Daniyal";
+// console.log("Thanks, " + userName + "!");
+
+
+
